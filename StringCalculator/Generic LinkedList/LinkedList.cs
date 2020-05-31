016d91ff11b03af8dbd72071bed87e031f246db8
Michael Rademeyer <michaelrademeyer@hotmail.com>
2020-05-31 11:24:27 +0200

    Added LinkedList

---
+++ StringCalculator/Generic LinkedList/LinkedList.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringCalculator.Generic_LinkedList
+{
+    public class LinkedList<T>
+    {
+        public Node<T> head;
+
+        public LinkedList(Node<T> node)
+        {
+            head = node;
+        }
+
+        public void PrintList()
+        {
+            Node<T> current = head;
+            if (current == null)
+            {
+                Console.WriteLine("No nodes in list");
+            }
+            else
+            {
+                while (current != null)
+                {
+                    Console.WriteLine(current.data);
+                    current = current.next;
+                }
+            }
+        }
+        public void Insert(T data)
+        {
+            Node<T> toAdd = new Node<T>
+            {
+                data = data
+            };
+            Node<T> current = head;
+            while (current != null)
+            {
+                current = current.next;
+            }
+            current.next = toAdd;
+        }
+        public void Delete(int position)
+        {
+            int foundPosition = 0;
+            Node<T> current = head;
+            Node<T> savedNodes = new Node<T>();
+            while (current != null)
+            {
+                if (foundPosition == position)
+                {
+                    savedNodes = current.next;
+                    break;
+                }
+                current = current.next;
+                foundPosition++;
+            }
+            current = head;
+            foundPosition = 0;
+            while (current != null)
+            {
+                if (foundPosition + 1 == position)
+                {
+                    current.next = savedNodes;
+                    break;
+                }
+                current = current.next;
+                foundPosition++;
+            }
+        }
+    }
+}
---
+++ StringCalculator/Generic LinkedList/Node.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringCalculator.Generic_LinkedList
+{
+    public class Node<T>
+    {
+        public Node<T> next;
+        public T data;
+    }
+}
